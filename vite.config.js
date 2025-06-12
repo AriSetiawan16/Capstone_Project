16f21915f2c503b51ba1fdf15a8dabd82ea5bc0c
amdzz <129802754+amdzz@users.noreply.github.com>
2025-06-12 09:40:33 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,34 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
+import path from 'path';
+
+const getFiles = (dir, ext, fileList = []) => {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        const filePath = path.join(dir, file);
+        if (fs.statSync(filePath).isDirectory()) {
+            getFiles(filePath, ext, fileList);
+        } else if (path.extname(file) === ext) {
+            fileList.push(filePath);
+        }
+    });
+    return fileList;
+};
+
+const cssFiles = getFiles('resources/css', '.css');
+const jsFiles = getFiles('resources/js', '.js');
+
+// Get ASSET_URL from env
+const assetUrl = process.env.ASSET_URL ?? '';
 
 export default defineConfig({
+    base: assetUrl ? `${assetUrl}/build/` : '/build/', // Important fix here!
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            publicDirectory: 'public',
+            input: [...cssFiles, ...jsFiles],
+            refresh: false,
         }),
-        tailwindcss(),
     ],
 });
